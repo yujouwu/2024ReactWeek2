67e879ef41e29d16e7bb00bfbee03d51e229cd56
sonia <soniayjw@gmail.com>
2025-01-07 23:52:46 +0800

    setup base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/2024ReactWeek2/" : "/",
   plugins: [react()],
 })
